751a0ca9644f9224f6c273010fa83c7f04438972
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-07-19 15:12:10 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -9,3 +9,15 @@ class Solution {
         return answer;
     }
 }
+
+
+// 이진법으로도 풀이가능// n을 <<로 1번 옮길때마다 2배니까 t만큼 옮겨주면된다.
+// class Solution {
+//     public static int solution(int n, int t) {
+//         int answer = 0;
+
+//         answer = n << t;
+
+//         return answer;
+//     }
+// }
