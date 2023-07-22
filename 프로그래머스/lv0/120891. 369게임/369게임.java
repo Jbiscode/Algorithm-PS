de32f21b7dd6d1be8a6d7c302d03286f9ad87b5b
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-07-22 16:56:06 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -1,10 +1,26 @@
+// class Solution {
+//     public int solution(int order) {
+//         int answer = 0;
+//         String num = Integer.toString(order);
+//         for(int i=0; i<num.length(); i++){
+//             if(num.charAt(i)=='3' || num.charAt(i)=='6' || num.charAt(i)=='9'){
+//                 answer +=1;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(int order) {
         int answer = 0;
         String num = Integer.toString(order);
         for(int i=0; i<num.length(); i++){
-            if(num.charAt(i)=='3' || num.charAt(i)=='6' || num.charAt(i)=='9'){
+            if(num.charAt(i)%3==0){
                 answer +=1;
+                if(num.charAt(i)=='0'){
+                    answer -=1;
+                }
             }
         }
         return answer;
