8f5fa4614831a8c8408e8dd3ccea844687b825c7
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-07-24 19:39:59 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,16 +1,32 @@
+// class Solution {
+//     public int solution(int chicken) {
+//         int answer = 0;
+//         int coupon = chicken;
+        
+//         while(coupon != 0){
+//             int service = coupon / 10;
+//             if(service == 0){
+//                 break;
+//             }
+//             answer += service;
+//             int restCoupon = coupon % 10;
+//             coupon = service + restCoupon;
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        int coupon = chicken;
-        
-        while(coupon != 0){
-            int service = coupon / 10;
-            if(service == 0){
-                break;
-            }
+
+        while(chicken >= 10){
+            int service = chicken / 10;
+            int bonus = chicken % 10;
+            
+            chicken = service + bonus;
+            
             answer += service;
-            int restCoupon = coupon % 10;
-            coupon = service + restCoupon;
         }
         return answer;
     }
