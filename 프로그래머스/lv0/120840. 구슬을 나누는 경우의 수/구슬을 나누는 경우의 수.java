95892e8dfcd10badb02c2ea1fe7ba7b1e0fcce42
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-07-25 23:51:50 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 369.70 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public static int recursion(int a, int b){
+        if(a==b || b==0){
+            return 1;
+        }
+        return recursion(a-1,b-1) + recursion(a-1,b);
+    }
+    
+    public int solution(int balls, int share) {
+        int answer = 0;
+        
+        answer = recursion(balls,share);
+        
+        return answer;
+    }
+}
