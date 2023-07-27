6f96550ed3a21f228eb4bfaea26ff7b28be60464
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-07-28 01:56:39 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
+        int cost = 12000*n + 2000*k;
+        int discount = 0;
+        if(n>=10){
+            discount = 2000*(n/10);
+        }
+        answer = cost-discount;
+        
         return answer;
     }
 }
