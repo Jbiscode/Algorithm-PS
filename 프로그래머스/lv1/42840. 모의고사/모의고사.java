0dee016468c458bbeb66ef4cb85d9b498a60adb9
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-07-28 13:28:43 +0900

    [level 1] Title: 모의고사, Time: 1.17 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -2,29 +2,28 @@ import java.util.*;
 class Solution {
     public static int[] solution(int[] answers) {
         ArrayList<Integer> tmp = new ArrayList<>();
-        int[] stu1 = {1, 2, 3, 4, 5};
+        int[] stu1 = {1, 2, 3, 4, 5};          // 학생들이 찍는 루틴 배열화
         int[] stu2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] stu3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] cnt = new int[3];
-        int answersLen = answers.length;
+        int[] cnt = new int[3];  //학생별 문제 맞춘갯수 나열
 
-        for (int i = 0; i < answersLen; i++) {
+        for (int i = 0; i < answers.length; i++) {  //찍는 루틴반복해서 정답지 비교
             if (answers[i] == stu1[i%5]){
                 cnt[0] += 1;
             }
         }
-        for (int i = 0; i < answersLen; i++) {
+        for (int i = 0; i < answers.length; i++) {
             if (answers[i] == stu2[i%8]){
                 cnt[1] += 1;
             }
         }
-        for (int i = 0; i < answersLen; i++) {
+        for (int i = 0; i < answers.length; i++) {
             if (answers[i] == stu3[i%10]){
                 cnt[2] += 1;
             }
         }
 
-        if (cnt[0] > cnt[1] && cnt[0] > cnt[2]){
+        if (cnt[0] > cnt[1] && cnt[0] > cnt[2]){  //최다 정답자 찾기 조건문
             tmp.add(1);
         } else if (cnt[1] > cnt[0] && cnt[1] > cnt[2]) {
             tmp.add(2);
@@ -47,7 +46,7 @@ class Solution {
 
 
         int idx = 0;
-        int[] answer = new int[tmp.size()];
+        int[] answer = new int[tmp.size()];  // ArrayList에 있는 최다정답자 answer[]에 옮기기
         for (Integer a :
                 tmp) {
             answer[idx++] = a;
@@ -56,3 +55,5 @@ class Solution {
         return answer;
     }
 }
+
+// 이 문제는 해설을 안보고 푼 첫 Lv1문제다!!
