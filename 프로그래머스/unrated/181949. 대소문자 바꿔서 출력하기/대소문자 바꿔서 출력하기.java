d60ade6227dbce1098471474a8e0c9a799418f6c
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-08-01 15:03:41 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 148.93 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -18,6 +18,24 @@ public class Solution {
 
             System.out.print(convert);
         }
-
     }
 }
+// 변환을 하고 값을 집어넣었다가 다시 변환했는데 알아서 아스키코드값으로 바뀌나보다.
+// 다시 문자열로 바뀔때만 (char)을 추가해서 문자로 바꾸자
+
+// import java.util.*;
+
+// public class Solution {
+//     public static void main(String[] args) { 
+//         Scanner sc = new Scanner(System.in); 
+//         String a = sc.next();
+//         for(int i = 0; i < a.length(); i++){
+//             if(a.charAt(i) >= 65 && a.charAt(i) <= 90){ 
+//                 System.out.print((char)(a.charAt(i) + 32));
+//             }
+//             else{ 
+//                 System.out.print((char)(a.charAt(i) - 32));
+//             }
+//         }
+//     } 
+// }
