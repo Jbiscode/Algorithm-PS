5a21ea06bf131e2f6512c0a0d6852d1523686186
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-08-01 14:00:03 +0900

    [unrated] Title: 문자열 반복해서 출력하기, Time: 175.91 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
