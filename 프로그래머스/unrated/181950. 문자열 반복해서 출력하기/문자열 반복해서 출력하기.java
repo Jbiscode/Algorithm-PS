691b7221312e78c1a47f60cdc0fbe8a2064debb2
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-08-01 13:57:28 +0900

    [unrated] Title: 문자열 반복해서 출력하기, Time: 197.87 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        String[] arr =  s.split(" ");
+        String a = arr[0];
+        int b = Integer.parseInt(arr[1]);
+
+        for (int i = 0; i <b ; i++) {
+            System.out.print(a);
+        }
+    }
+}
+
+// Scanner로 받아오면 간단하지만 BufferedReader는 모두 String으로 받아와서 그뒤에 데이터를 바꿔야함
+// 그리고 util인 scanner와 다르게 BufferReader는 IOexcption이 필수로 필요하다.
+
+
+// Scanner를 사용한 예시
+
+// import java.util.*; 
+// public class Solution {
+//     public static void main(String[] args) { 
+//         Scanner sc = new Scanner(System.in); 
+//         String str = sc.next();
+//         int n = sc.nextInt();
+//         for(int i = 0; i < n; i++){ 
+//             System.out.print(str);
+//         } 
+//     }
+// }
