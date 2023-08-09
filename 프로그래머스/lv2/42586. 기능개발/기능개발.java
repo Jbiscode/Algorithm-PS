19043d852f933c0fbe6f5db97afefd3f5763e240
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-08-09 12:45:33 +0900

    [level 2] Title: 기능개발, Time: 0.14 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        int day;
+        int count = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            day = 0;
+            while (progresses[i] + day * speeds[i] < 100) {
+                day++;
+            }
+            queue.offer(day);
+        }
+        int tmp = queue.peek();
+        while (!queue.isEmpty()) {
+            if (queue.peek() <= tmp) {
+                queue.poll();
+            } else {
+                tmp = queue.poll();
+            }
+            count++;
+
+            if (queue.isEmpty()) {
+                arr.add(count);
+            } else if (queue.peek() > tmp) {
+                arr.add(count);
+                count = 0;
+                tmp = 0;
+            }
+        }
+        answer = new int[arr.size()];
+        int idx = 0;
+        for (int num :
+                arr) {
+            answer[idx++] = num;
+        }
+        return answer;
+    }
+}
