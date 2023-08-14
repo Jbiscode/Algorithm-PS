011948505dd0caf55d0a00eae3b4f5856beeb9c9
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-08-14 15:14:06 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,23 +1,22 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n) {
+        int[] answer = {};
         int len = 0;
-        if(n%2==0){
-            len = n/2;
+        if(n%2 == 0){
+            len = n / 2;
         }else{
-            len = (n+1)/2;
+            len = n/2 + 1;
         }
         
-        int[] answer = new int[len];
-        ArrayList<Integer> tmp = new ArrayList<>();
-        
-        for(int i=1; i<=n;i+=2){
-            tmp.add(i);
-        }
+        answer = new int[len];
         int idx = 0;
-        for(Integer num:tmp){
-            answer[idx++] = num;
+        for(int i = 1; i <= n; i++){
+            if(i%2 != 0){
+                answer[idx] = i;
+                idx++;
+            }
         }
+        
         return answer;
     }
 }
