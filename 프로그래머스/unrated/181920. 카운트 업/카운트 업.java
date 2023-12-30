336c3ce71e044953f56b3c4eed2c76ed89f0a6b1
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-12-30 22:23:02 +0900

    [level unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,13 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
-    public int[] solution(int start, int end) {
-        int[] answer = {};
-        int len = end - start + 1 ;
-        answer = new int[len];
+    public int[] solution(int start_num, int end_num) {
+        int len = end_num - start_num + 1;
         
-        for(int i=0;i<len;i++){
-            answer[i]=start + i;
+        int[] answer = new int[len];
+        
+        for (int i = 0; i < len; i++) {
+            answer[i] = start_num++;
         }
-        
         return answer;
     }
 }
