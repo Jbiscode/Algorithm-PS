628e9106aa74c498a947df27dfc46df4f2a2876c
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2023-12-31 17:28:24 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        int len = myString.length();
+        int patLen = pat.length();
+        
+        for (int i = 0; i < len - patLen + 1; i++) {
+            if (myString.substring(i, i + patLen).equals(pat)) {
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+}
