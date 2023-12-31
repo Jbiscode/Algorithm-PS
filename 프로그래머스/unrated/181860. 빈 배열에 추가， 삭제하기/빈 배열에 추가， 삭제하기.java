5f71b554dd4cdb5f9d8bdd152662313f1be16ce5
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-01-01 00:24:51 +0900

    [level unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, boolean[] flag) {
+        ArrayList<Integer> arrList = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (flag[i]) {
+                for (int j = 0; j < arr[i] * 2; j++) {
+                    arrList.add(arr[i]);
+                }
+            } else {
+                for (int j = 0; j < arr[i]; j++) {
+                    arrList.remove(arrList.size() - 1);
+                }
+            }
+        }
+
+        int[] answer = new int[arrList.size()];
+        
+        for (int i = 0; i < arrList.size(); i++) {
+            answer[i] = arrList.get(i);
+        }
+
+        return answer;
+    }
+
+}
