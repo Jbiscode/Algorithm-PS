60c734dfd6464e34f17abf7022bebe99bc34c765
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-01-01 10:57:37 +0900

    [level unrated] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[][] arr) {
+        int answer = 0;
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == arr[j][i]) {
+                    cnt++;
+                }
+            }
+        }
+
+        if (cnt == (arr.length * arr.length)) {
+            answer = 1;
+        }
+        return answer;
+    }
+}
