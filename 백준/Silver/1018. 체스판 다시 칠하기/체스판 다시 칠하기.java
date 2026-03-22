3b9a8ed1d7a226bb64a6da0c47ce816821cea2f9
JaeBin <99472056+Jbiscode@users.noreply.github.com>
2026-03-22 21:16:03 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 11764 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -7,8 +7,6 @@ import java.util.StringTokenizer;
 public class Main {
     static StringBuilder sb;
     static char[][] grid;
-    static int blackCnt, whiteCnt;
-    static char[] boardLine;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -21,61 +19,15 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         grid = new char[N][M];
-        blackCnt = 0;
-        whiteCnt = 0;
         int answer = Integer.MAX_VALUE;
 
         for (int i = 0; i < N; i++) {
-            boardLine = br.readLine().toCharArray();
-            System.arraycopy(boardLine, 0, grid[i], 0, M);
+            grid[i] = br.readLine().toCharArray();
         }
 
         for (int i = 0; i <= N-8; i++) {
             for (int j = 0; j <= M-8; j++) {
-
-                for (int k = i; k < i+8; k++) {
-                    for (int l = j; l <j+8; l++) {
-                        if(k % 2 == 0){
-                            if (l % 2 == 0){
-                                if (!(grid[k][l] == 'W')){ // 맨처음 화이트시작일때 화이트가아니면 바꿔줘야함
-                                    whiteCnt++;
-                                }
-                                if (!(grid[k][l] == 'B')){ // 맨처음 블랙시작일때 블랙이아니면 바꿔줘야함
-                                    blackCnt++;
-                                }
-                            }else {
-                                if (grid[k][l] == 'W'){ // 맨처음 화이트시작일때 옆이 화이트면 바꿔줘야함
-                                    whiteCnt++;
-                                }
-                                if (grid[k][l] == 'B'){ // 맨처음 블랙시작일때 옆이 블랙이면 바꿔줘야함
-                                    blackCnt++;
-                                }
-                            }
-                        }else {
-                            if (l % 2 == 0){
-                                // 화이트시작일때, 두번째줄 시작은 블랙임 화이트면 바꿔줘야함
-                                if(grid[k][l] == 'W'){
-                                    whiteCnt++;
-                                }
-                                if(grid[k][l] == 'B'){
-                                    blackCnt++;
-                                }
-                            }else{
-                                if(!(grid[k][l] == 'W')){
-                                    whiteCnt++;
-                                }
-                                if(!(grid[k][l] == 'B')){
-                                    blackCnt++;
-                                }
-                            }
-
-                        }
-                    }
-                }
-                int temp = Math.min(whiteCnt, blackCnt);
-                answer = Math.min(temp, answer);
-                whiteCnt = 0;
-                blackCnt = 0;
+                answer = Math.min(answer, cntColor(i,j));
             }
         }
 
@@ -83,5 +35,20 @@ public class Main {
 
         bw.write(sb.toString());
         bw.flush();
+    }
+
+    private static int cntColor(int i, int j) {
+        int count = 0;
+
+        for (int k = i; k < i + 8; k++) {
+            for (int l = j; l < j + 8; l++) {
+                char expectedChar = (k+l) % 2 == 0 ? 'W' : 'B';
+                if (grid[k][l] != expectedChar){
+                    count++;
+                }
+            }
+        }
+        // 총 갯수는 같으니 하나의 케이스만 구하고, 반대로 구할 수 있음
+        return Math.min(count, 64 - count);
     }
 }
